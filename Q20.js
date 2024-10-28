e833a99d448242a709a2c9d9487fcfa15583c4cf
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-28 23:21:03 +0530

    Create Q20.js

---
+++ Q20.js
@@ -0,0 +1,7 @@
+// Write a JavaScript Program to Calculate the Area of a Triangle.
+function findSquareRoot(num) {
+    return Math.sqrt(num);
+}
+
+let number = 16;
+console.log("Square Root: " + findSquareRoot(number));
