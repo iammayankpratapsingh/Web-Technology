2e48f5d980caf77adaeb9fa2723b2a758d8b22d5
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-28 23:20:22 +0530

    Create Q19.js

---
+++ Q19.js
@@ -0,0 +1,8 @@
+//Q19. Write a JavaScript Program to Find the Square Root.
+
+function findSquareRoot(num) {
+    return Math.sqrt(num);
+}
+
+let number = 16;
+console.log("Square Root: " + findSquareRoot(number));
