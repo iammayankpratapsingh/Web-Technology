6a4d8d3cd553cab7ba796d636087b4e3ebf2b4a9
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-28 23:19:26 +0530

    Create Q18.js

---
+++ Q18.js
@@ -0,0 +1,7 @@
+//Q18. Write a JavaScript Program to Add Two Numbers.
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let num1 = 5;
+let num2 = 10;
+console.log("Sum: " + addTwoNumbers(num1, num2));
