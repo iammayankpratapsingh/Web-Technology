24f977c24393eac1d2995ce7c8d78740a5ae914f
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-29 12:13:15 +0530

    Create Q24.js

---
+++ Q24.js
@@ -0,0 +1,7 @@
+// Write a JavaScript Program to Convert Decimal to Binary
+function decimalToBinary(decimal) {
+    return decimal.toString(2);
+}
+
+let decimal = 10;
+console.log("Binary: " + decimalToBinary(decimal));
