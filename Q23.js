3a449976f2451596d0ca46fee2e4d881911c4bfb
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-29 12:12:13 +0530

    Create Q23.js

---
+++ Q23.js
@@ -0,0 +1,7 @@
+// Write a JavaScript Program to Convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+let celsius = 25;
+console.log(celsius + "°C is " + celsiusToFahrenheit(celsius) + "°F");
