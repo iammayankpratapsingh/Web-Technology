e532ccea9934eb763a1aefc28b9056476dbc19d9
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-10-29 12:16:54 +0530

    Create Q28.js

---
+++ Q28.js
@@ -0,0 +1,9 @@
+// JavaScript Program to Display the Multiplication Table
+function multiplicationTable(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${n} * ${i} = ${n * i}`);
+    }
+}
+
+let num = 5;
+multiplicationTable(num);
