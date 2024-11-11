9b4d53403f679ae9f1a792e8b2a4fadb6d221508
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-11-11 19:00:14 +0530

    Create Q39.js

---
+++ Q39.js
@@ -0,0 +1,5 @@
+// 39. Write a program to create dialogue boxes using JavaScript.
+alert("This is an alert box!");
+const userResponse = confirm("Do you want to proceed?");
+const userName = prompt("Please enter your name:");
+console.log(`Response: ${userResponse}, Name: ${userName}`);
