3ff1326f022d17c218aa9991c97118152ea2ef8d
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-11-11 19:09:21 +0530

    Create Q47.js

---
+++ Q47.js
@@ -0,0 +1,5 @@
+// 47. JavaScript Program to Check Leap Year
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+console.log(isLeapYear(2024)); // Example usage
