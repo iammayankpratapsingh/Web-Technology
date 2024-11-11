34653e1a3ab28627f753d7116bbe12bbc46829a3
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-11-11 18:50:40 +0530

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,9 @@
+// 31. JavaScript Program to Find the Sum of Natural Numbers
+function sumOfNaturalNumbers(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumOfNaturalNumbers(10));
